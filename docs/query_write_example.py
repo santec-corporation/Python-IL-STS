7e59f70100df24632bc17f4d7524f4916003fbdc
r17_iNSANE <rakshith.pj@santec.com>
2024-10-25 17:49:14 +0900

    Update query write example file.

--- docs/query_write_example.py
+++ docs/query_write_example.py
@@ -7,7 +7,7 @@ Created on Wed 05 17:17:26 2024
 @organization: santec holdings corp.
 """
 
-# Importing high level santec package and its modules
+# Importing high-level santec package and its modules
 from santec import TslInstrument, MpmInstrument, GetAddress
 
 # Initializing get instrument address class
@@ -17,24 +17,19 @@ device_address = GetAddress()
 def main():
     """ Main method of this project """
 
-    tsl = None
-    mpm = None
+    tsl: TslInstrument
+    mpm: MpmInstrument
 
     device_address.initialize_instrument_addresses()
     tsl_address = device_address.get_tsl_address()
     mpm_address = device_address.get_mpm_address()
-    interface = 'GPIB'
 
     # Only connect to the devices that the user wants to connect
-    if tsl_address is not None:
-        tsl = TslInstrument(interface, tsl_address)
-        tsl.connect()
-    else:
-        raise Exception("There must be a TSL connected")
+    tsl = TslInstrument(instrument=tsl_address)
+    tsl.connect()
 
-    if mpm_address is not None:
-        mpm = MpmInstrument(interface, mpm_address)
-        mpm.connect()
+    mpm = MpmInstrument(instrument=mpm_address)
+    mpm.connect()
 
     # TSL Query / Write example
     # Write to TSL
@@ -43,7 +38,7 @@ def main():
 
     # Query TSL
     status, response = tsl.query_tsl('POW?')        # Gets TSL output power
-    print(status)             # Prints status 0 if query was successful
+    print(status)             # Prints status 0 if a query was successful
     print(response)           # Prints query response
 
     # MPM Query / Write example
@@ -53,7 +48,7 @@ def main():
 
     # Query MPM
     status, response = mpm.query_mpm('AVG?')  # Gets MPM averaging time
-    print(status)  # Prints status 0 if query was successful
+    print(status)  # Prints status 0 if a query was successful
     print(response)  # Prints query response
 
 
