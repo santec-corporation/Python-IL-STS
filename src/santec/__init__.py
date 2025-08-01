003f5c25057de1e76e4ad4289c145222cdec35d7
rpj17-santec <rakshith.pj@santec.com>
2025-08-01 16:45:45 +0900

    Increment project version to 2.9.0.

--- src/santec/__init__.py
+++ src/santec/__init__.py
@@ -10,19 +10,18 @@ import os
 import logging
 import datetime
 import argparse
-import ctypes
 import sys
 import platform
 import clr
 
 # About
-__version__ = "2.8.20"
+__version__ = "2.9.0"
 __author__ = "Chentir MT"
 __project_name__ = "Santec_IL_STS"
 __organization__ = "Santec Holdings Corporation"
 __description__ = "Program to measure the Insertion Loss using the Swept Test System"
 __url__ = "https://github.com/santec-corporation/Santec_IL_STS"
-__date__ = "2025-06-11"
+__date__ = "2025-08-01"
 __license__ = "GNU General Public License v3.0"
 __copyright__ = f"Copyright 2021-{datetime.date.today().year}, {__organization__}"
 
