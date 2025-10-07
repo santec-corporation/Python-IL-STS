0c9856989ec271f64ac9af696baa6257b930e89b
rpj17-santec <rakshith.pj@santec.com>
2025-10-07 11:14:38 +0900

    Add dll manager to load dlls locally

--- python_il_sts/__init__.py
+++ python_il_sts/__init__.py
@@ -4,40 +4,34 @@ Python IL STS process.
 @organization: Santec Holdings Corp.
 """
 
-import os
-import clr
+
+from .drivers import load_dlls
 
 # Get and initialize the logger
 from .logger import get_logger
 logger = get_logger(__name__)
 
-# Add the Santec DLLs to the root.
-ROOT = str(os.path.dirname(__file__)) + '\\DLL\\'
-logger.info("Getting DLL path, root: %s", ROOT)
-# print(ROOT)    # Uncomment in to check if the root was selected properly
-
-DLL1 = 'Santec.Instrument'
-DLL2 = 'STSProcess'
-result1 = clr.AddReference(ROOT + DLL1)  # Add the Instrument DLL to the root
-result2 = clr.AddReference(ROOT + DLL2)  # Add the STSProcess DLL to the root
-logger.info("Adding Instrument DLL to the root, result: %s", result1)
-logger.info("Adding STSProcess DLL to the root, result: %s", result2)
-# print(result1, result2)     # Comment in to check if the DLLs were added.
+try:
+    # Initialize and Load the Santec DLLs
+    setup_dlls_result = load_dlls()
+    logger.info("Santec DLLs loaded successfully.")
+except Exception as e:
+    logger.error("Error while Santec DLLs: ", str(e))
+    raise
 
 
 # Import santec modules
-import file_saving
 from .get_address import GetAddress
 from .sts_process import StsProcess
 from .daq_device_class import SpuDevice
 from .tsl_instrument_class import TslInstrument
 from .mpm_instrument_class import MpmInstrument
 
+
 __all__ = [
     "StsProcess",
     "TslInstrument",
     "MpmInstrument",
     "SpuDevice",
-    "GetAddress",
-    "file_saving"
+    "GetAddress"
 ]
---
+++ python_il_sts/drivers/__init__.py
@@ -0,0 +1,12 @@
+# python_il_sts/drivers/__init__.py
+
+"""
+Drivers module.
+"""
+
+from .dll_manager import load_dlls
+
+
+__all__ = [
+    "load_dlls"
+]
