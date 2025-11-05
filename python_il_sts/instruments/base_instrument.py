9987f9e9b2f5e492247ed86114017f15731c3915
rpj17-santec <rakshith.pj@santec.com>
2025-11-05 16:13:23 +0900

    Expose instrument information class

--- python_il_sts/instruments/base_instrument.py
+++ python_il_sts/instruments/base_instrument.py
@@ -18,6 +18,12 @@ class BaseInstrument:
     def instrument(self):
         return self._instrument
 
+    @property
+    def information(self):
+        if self._instrument:
+            return self._instrument.Information
+        raise Exception("Instrument is not connected.")
+
     def _check_restricted_method(self):
         """Check if the method is restricted to certain instrument types."""
         if not isinstance(self._instrument, (TSL, MPM)):
